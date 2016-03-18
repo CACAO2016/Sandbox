8f65a4721f2f3acc177193fa5fddd8ed1cc1efc5
jhtr <jhtr@users.noreply.github.com>
2016-03-18 16:09:17 +0100

    Add Group1 class

---
+++ SandBox/src/prime2016/Group1.java
@@ -0,0 +1,12 @@
+package prime2016;
+
+public class Group1 {
+
+	public static void printNames() {
+		System.out.println("jhtr");
+	}
+	
+	public static void main(String[] args) {
+		printNames();
+	}
+}
