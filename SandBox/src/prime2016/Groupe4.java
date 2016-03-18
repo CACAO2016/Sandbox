2f6ad97fb1dc942cfb42815fded3fc60f2f3fc8d
Sofiane <salort.sofiane@gmail.com>
2016-03-18 16:11:48 +0100

    SofianeSalort

---
+++ SandBox/src/prime2016/Groupe4.java
@@ -0,0 +1,14 @@
+package prime2016;
+
+public class Groupe4 {
+	
+	public static void NameGroupe4(String[] args) {
+		System.out.println("SofianeSalort");
+
+	}
+
+	public static void main(String[] args) {
+		NameGroupe4(args);
+	}
+
+}
